b337b14c2fc2a897e9fe6d39802dfb66dfcf1e56
okuoku <mjt@cltn.org>
2022-04-02 20:28:39 +0900

    Add `uxTopUsedPriority` symbol for OpenOCD

--- blink.cpp
+++ blink.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 
+#include "FreeRTOS.h"
+/* OpenOCD stub */
+const volatile UBaseType_t uxTopUsedPriority = configMAX_PRIORITIES - 1U;
+
 void setup() {
+    (void) uxTopUsedPriority;
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
